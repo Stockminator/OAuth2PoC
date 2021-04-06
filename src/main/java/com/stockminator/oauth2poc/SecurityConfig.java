de0165c13db15905157bf8f91e943559f438965e
attilapalfi92 <apalfi@cloudera.com>
2021-04-06 23:57:50 +0200

    typo fix

--- src/main/java/com/stockminator/oauth2poc/SecurityConfig.java
+++ src/main/java/com/stockminator/oauth2poc/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
 
                 .authorizeExchange()
